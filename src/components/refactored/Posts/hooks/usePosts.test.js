7e20b4a801b6c377bca4a9daec6b1c43d0e3386d
Balazs Bereczky <bereczkybalazs1@gmail.com>
2024-07-11 15:30:37 +0200

    test refactor

--- src/components/refactored/Posts/hooks/usePosts.test.js
+++ src/components/refactored/Posts/hooks/usePosts.test.js
@@ -5,6 +5,21 @@ const mockFetch = jest.fn()
 // Mock fetch
 global.fetch = mockFetch
 
+
+const expectPostsToBe = async (expectedPosts) => {
+  const { result, rerender } = renderHook(usePosts)
+
+  expect(result.current.posts).toEqual([])
+
+  rerender()
+  await waitFor(() => {
+    expect(result.current.posts).toEqual(expectedPosts)
+  })
+
+  expect(fetch).toHaveBeenCalledTimes(1)
+  expect(fetch).toHaveBeenCalledWith('https://dummyjson.com/posts')
+}
+
 describe('usePosts', () => {
 
   it('should fetch and set posts', async () => {
@@ -13,38 +28,13 @@ describe('usePosts', () => {
     mockFetch.mockReturnValue(Promise.resolve({
       json: () => Promise.resolve({posts: expectedPosts})
     }))
-    const { result, rerender } = renderHook(usePosts)
 
-    expect(result.current.posts).toEqual([])
-
-    rerender()
-    await waitFor(() => {
-      expect(result.current.posts).toEqual(expectedPosts)
-    })
-
-    expect(fetch).toHaveBeenCalledTimes(1)
-    expect(fetch).toHaveBeenCalledWith('https://dummyjson.com/posts')
+    await expectPostsToBe(expectedPosts)
   })
 
   it('should handle fetch errors', async () => {
-    // Mock fetch to reject
     mockFetch.mockReturnValue(() => Promise.reject('API is down'))
 
-    const { result, rerender } = renderHook( usePosts)
-
-    // Check initial state
-    expect(result.current.posts).toEqual([])
-
-    // Wait for the fetch to be complete
-    rerender()
-
-    // Check the posts state remains empty after fetch failure
-    await waitFor(() => {
-      expect(result.current.posts).toEqual([])
-    })
-
-    // Ensure fetch was called once
-    expect(fetch).toHaveBeenCalledTimes(1)
-    expect(fetch).toHaveBeenCalledWith('https://dummyjson.com/posts')
+    await expectPostsToBe([])
   })
 })
