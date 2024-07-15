638ac5a3c4e1d68192b2309f1c876fb61bd7d0e6
Balazs Bereczky <bereczkybalazs1@gmail.com>
2024-07-15 13:59:53 +0200

    make refactored posts visible in refactored posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Posts } from "./components/original/Posts/Posts";
-import { Posts as RefactoredPosts } from "./components/original/Posts/Posts";
+import { Posts as RefactoredPosts } from "./components/refactored/Posts/Posts";
 
 export const App = () => {
   return (
